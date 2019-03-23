fe437a1f4e2c3c4354ef316d630731d57bd19540
Timothy Robertson <timr@sfu.ca>
2019-03-23 18:54:59 -0400

    Add canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as box2d from '@flyover/box2d';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'slothroyale';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('canvas') public canvas: ElementRef;
+
+  private ctx: CanvasRenderingContext2D;
+
+  public ngAfterViewInit() {
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.ctx = canvasEl.getContext('2d');
+  }
 }
