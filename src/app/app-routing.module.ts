d3fe1d8cc118db92adf0471b0182f8a9f390dabb
Timothy Robertson <timr@sfu.ca>
2019-11-15 00:51:10 -0800

    Split App into Singleplayer, Multiplayer modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'singleplayer',
+    loadChildren: () => import('./modules/singleplayer/singleplayer.module').then(mod => mod.SingleplayerModule)
+  },
+  {
+    path: 'multiplayer',
+    loadChildren: () => import('./modules/multiplayer/multiplayer.module').then(mod => mod.MultiplayerModule)
+  },
+  {
+    path: '',
+    redirectTo: 'singleplayer',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/multiplayer/multiplayer.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { MultiplayerComponent } from './pages/multiplayer/multiplayer.component';
+
+export const routes: Routes = [
+  { path: '', component: MultiplayerComponent },
+];
